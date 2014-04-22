84226f3439214d6bf51bc08c4617e46a6f6f294e
practicasweb <practicasweb@worker11>
2014-04-22 19:22:07 +0200

    css & js aplicated erros on view

--- mod_carousel/mod_carousel.php
+++ mod_carousel/mod_carousel.php
@@ -1,7 +1,15 @@
 <?php
-defined( '_JEXEC' ) or die( 'Restricted access' );
-require_once( dirname(__FILE__).DS.'helper.php' );
 
-$carousel = modCarouselHelper::getCarousel( $params );
-require( JModuleHelper::getLayoutPath( 'mod_carousel' ) );
+defined('_JEXEC') or die('Restricted access');
+require_once( dirname(__FILE__) . DS . 'helper.php' );
+
+$document =& JFactory::getDocument();
+
+$document->addStyleSheet('modules/mod_carousel/assets/css/bootstrap.min.css');
+$document->addStyleSheet('modules/mod_carousel/assets/css/mod_carousel.css');
+$document->addScript('modules/mod_carousel/assets/js/bootstrap.min.js');
+
+$carousel = modCarouselHelper::getCarousel($params);
+require( JModuleHelper::getLayoutPath('mod_carousel') );
+
 ?>
--- mod_carousel/tmpl/default.php
+++ mod_carousel/tmpl/default.php
@@ -1,29 +1,29 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access');
 ?>
-<?php echo '' ?><div class="carousel_container">
-    <div class="imgFondoOdin">&nbsp;
+<div class="carousel_container">
+    <div class="containerModule">&nbsp;
         <div class="container fill ">
             <div id="myCarousel" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="active item">
-                        <div class="fill" style="background-image: url(' <?php $imagen01 ?>');">&nbsp;</div>
+                        <div class="fill" style="background-image: url('images/1900x1080_01.gif');">&nbsp;</div>
                         <div class="container">
                             <div class="carousel-caption">
-                                <h2><?php $caption01 ?></h2>
+                                <h2>Esto es un h2</h2>
                             </div>
                         </div>
                     </div>
                     <div class="item">
-                        <div class="fill" style="background-image: url(' <?php $imagen02 ?>');">&nbsp;</div>
+                        <div class="fill" style="background-image: url('images/1900x1080_02.gif');">&nbsp;</div>
                         <div class="carousel-caption">
-                            <h2><?php $caption02 ?></h2>
+                            <h2>esto es OTRO h2</h2>
                         </div>
                     </div>
                     <div class="item">
-                        <div class="fill" style="background-image: url(' <?php $imagen03 ?>');">&nbsp;</div>
+                        <div class="fill" style="background-image: url('images/1900x1080_03.gif');">&nbsp;</div>
                         <div class="carousel-caption">
-                            <h2><?php $caption03 ?></h2>
+                            <h2>Este es otro OTRO h2</h2>
                         </div>
                     </div>
                 </div>
@@ -31,5 +31,4 @@ defined('_JEXEC') or die('Restricted access');
             </div>
         </div>
     </div>
-</div><?php ;
-?>
+</div>
